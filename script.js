50dcb977ebf8119d9d37f766f963c3647439adce
Marcos Domingues <dfenixweb@gmail.com>
2020-10-14 11:13:49 -0300

    Movimento adicionado (rigth)

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ snake[0] = { //posição onde sera criado a cobra
     y: 8 * box
 }
 
+let direction = "right";
+
 function criarBG() { //cria o fundo
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);  
@@ -20,5 +22,31 @@ function criarSnake(){ //cria a cobra
 
 }
 
-criarBG();
-criarSnake();
+function iniciarJogo(){
+    criarBG();
+    criarSnake();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") {
+        snakeX += box;
+    }
+    if(direction == "left"){
+       snakeX -= box; 
+    } 
+    if(direction == "up") {
+        snakeY -= box;
+    }
+    if(direction == "down"){
+       snakeY += box; 
+    } 
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
